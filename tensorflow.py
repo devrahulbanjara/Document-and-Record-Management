b6818901eb95355e071526d73d92ca45a18b3a1c
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-08-07 13:18:02 +0545

    Experiment on yolov8 and rename directories

--- tensorflow.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
-print("CUDA version used by TensorFlow:", tf.sysconfig.get_build_info()["cuda_version"])
-print("cuDNN version used by TensorFlow:", tf.sysconfig.get_build_info()["cudnn_version"])
--- torch.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("PyTorch version:", torch.__version__)
-print("CUDA version used by PyTorch:", torch.version.cuda)
-print("cuDNN version used by PyTorch:", torch.backends.cudnn.version())
-print("PyTorch GPU availability:", torch.cuda.is_available())
