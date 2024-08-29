374638e0634634e7b189e2a138c497930861a8a1
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-08-29 20:44:02 +0545

    Main file to work on

---
+++ src/document_record/main.py
@@ -0,0 +1,10 @@
+import cv2
+import matplotlib.pyplot as plt
+import torch
+
+def app(img_path):
+    image = cv2.imread(img_path,0)
+    model
+
+if __name__ == "__main__":
+    app("/mnt/c/Users/Rahul/Desktop/Docs and Essentials/Confidential Passports/hajurba.JPG")
