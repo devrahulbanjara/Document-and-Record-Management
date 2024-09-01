8bd8ddad6bfd13343faf75230487e7bdd539a7ec
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-09-01 23:09:36 +0545

    Establish database connection

--- src/document_record/main.py
+++ src/document_record/main.py
@@ -1,10 +1,17 @@
-import cv2
-import matplotlib.pyplot as plt
-import torch
+import mysql.connector
 
-def app(img_path):
-    image = cv2.imread(img_path,0)
-    model
 
-if __name__ == "__main__":
-    app("/mnt/c/Users/Rahul/Desktop/Docs and Essentials/Confidential Passports/hajurba.JPG")
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="3241",
+    auth_plugin='mysql_native_password',
+    database="Docmgmt"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM License")
+
+for i in mycursor:
+    print(i)
